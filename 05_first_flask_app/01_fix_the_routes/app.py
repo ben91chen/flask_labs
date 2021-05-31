99ae1b214e40834085540317705ff6516dcf4aa4
ben91chen <bc9938@gmail.com>
2021-05-30 21:30:42 -0600

    Commit 05_lab

--- 05_first_flask_app/01_fix_the_routes/app.py
+++ 05_first_flask_app/01_fix_the_routes/app.py
@@ -2,18 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def welcome():
-    page = "Welcome to Emily's Dog Costumes!"
+    return "Welcome to Emily's Dog Costumes!"
 
-
-@app.route('services')
+@app.route('/services')
 def services():
-    page = "I offer custom made costumes for your precious canine companion, "\
-        "and a free in-home consultation, to get the measurements."
+    return "I offer custom made costumes for your precious canine companion, "\
+        "<br>and a free in-home consultation, to get the measurements."
 
 
-@app.route('/costumes/costume')
+@app.route('/costumes/<costume>')
 def costumes(costume):
-    page = f"Check out this {costume} costume!"
+    return f"Check out this {costume} costume!"
